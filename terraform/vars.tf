b28d7713bf02ccfb8f3c7dc875ff0e419cddeab1
dirkwall <dirk.wallerstorfer@dynatrace.com>
2018-04-11 20:38:35 +0200

    updated varibles

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,9 +1,17 @@
 variable "aws_region" {
-  default = ""
+  default = "us-west-1"
 }
 
-variable "aws_flavor" {
-  default = ""
+variable "aws_flavor_elastic" {
+  default = "t2.xlarge"
+}
+
+variable "aws_flavor_loadgen" {
+  default = "t2.small"
+}
+
+variable "aws_flavor_sockshop" {
+  default = "t2.xlarge"
 }
 
 variable "aws_keypair_name" {
